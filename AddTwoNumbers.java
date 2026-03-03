6f615b5d287504ff440be94db6ae403c1dc37d06
Rakesh2890 <rakesh.raki.3678@gmail.com>
2026-03-03 06:38:35 +0000

    initail commit

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,10 +1,10 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         
-        int num1 = 10;
+        int num1 = 30;
         int num2 = 20;
         
-        int sum = num1 + num2;
+        int sum = num1 + num2
         
         System.out.println("First Number: " + num1);
         System.out.println("Second Number: " + num2);
